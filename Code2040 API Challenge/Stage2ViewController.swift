93c88f92a9e8421ca1d50df8c607f8f03e3f6be7
Eude Kinsley Lesperance <l.keude96@gmail.com>
2015-01-08 20:37:47 -0600

    Passed Stage 2

--- Code2040 API Challenge/Stage2ViewController.swift
+++ Code2040 API Challenge/Stage2ViewController.swift
@@ -7,14 +7,79 @@
 //
 
 import UIKit
+import AlamoFire
+import SwiftyJSON
 
 class Stage2ViewController: UIViewController {
+    
+    @IBOutlet weak var needleLabel: UILabel!
+    @IBOutlet weak var haystackLabel: UILabel!
+    @IBOutlet weak var needleIndexLabel: UILabel!
+    let commonURL = "http://challenge.code2040.org/api"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let urlForHaystack = "\(self.commonURL)/haystack"
+        
+        let token = [
+            "token": "YwUoY4pc6I"
+            ] as Dictionary
+        
+        
+        Alamofire.request(.POST, urlForHaystack, parameters: token, encoding: .JSON)
+            .responseJSON { (request, response, json, error) in
+                if(error != nil) {
+                    println("Error: \(error)")
+                    println(request)
+                    println(response)
+                }
+                else {
+                    println("Success: \(urlForHaystack)")
+                    println(request)
+                    var json = JSON(json!)
+                    if let data = json["result"] as JSON?{
+                        var haystack = data["haystack"].arrayValue as Array
+                        println(haystack)
+                        var needle = data["needle"].stringValue as String?
+                        println(needle)
+                        
+                        var needleIndex = 0
+                        
+                        while(haystack[needleIndex].stringValue as String != needle){
+                            needleIndex++
+                        }
+                        
+                        let validateDict = [
+                            "token": "YwUoY4pc6I",
+                            "needle": "\(needleIndex)"
+                            ] as Dictionary
+                        
+                        let urlForValidateNeedle = "\(self.commonURL)/validateneedle"
+                        
+                        Alamofire.request(.POST, urlForValidateNeedle, parameters: validateDict, encoding: .JSON)
+                            .responseJSON { (request, response, json, error) in
+                                if(error != nil) {
+                                    println("Error: \(error)")
+                                    println(request)
+                                    println(response)
+                                }
+                                else {
+                                    println("Success: \(urlForValidateNeedle)")
+                                    println(request)
+                                    var json = JSON(json!)
+                                    if let data = json["result"].stringValue as String? {
+                                        println(data)
+                                    }
+                                }
+                        }
+                    }
+                }
+        }
     }
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -22,14 +87,12 @@ class Stage2ViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func fetchDataButton(sender: UIButton) {
     }
-    */
+    
+    @IBAction func findNeedleButton(sender: UIButton) {
+    }
 
+    @IBAction func submitButton(sender: UIButton) {
+    }
 }
