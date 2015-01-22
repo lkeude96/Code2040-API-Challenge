fb3b9354ea56240a62c03c64efee6bf054712a91
Eude Kinsley Lesperance <l.keude96@gmail.com>
2015-01-22 13:48:12 -0600

    Passed Stage 4

--- Code2040 API Challenge/Stage4ViewController.swift
+++ Code2040 API Challenge/Stage4ViewController.swift
@@ -7,13 +7,78 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class Stage4ViewController: UIViewController {
+    let commonURL = "http://challenge.code2040.org/api"
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        let urlForTime = "\(self.commonURL)/time"
+        
+        let token = [
+            "token": "YwUoY4pc6I"
+            ] as Dictionary
+        
+        
+        Alamofire.request(.POST, urlForTime, parameters: token, encoding: .JSON)
+            .responseJSON { (request, response, json, error) in
+                if(error != nil) {
+                    println("Error: \(error)")
+                    println(request)
+                    println(response)
+                }
+                else {
+                    println("Success: \(urlForTime)")
+                    println(request)
+                    var json = JSON(json!)
+                    if let data = json["result"] as JSON?{
+                        var dateStamp = data["datestamp"].stringValue as String
+                        println(dateStamp)
+                        var interval = data["interval"].doubleValue as NSTimeInterval
+                        println(interval)
+                        
+                        let dateFormat = NSDateFormatter()
+                            dateFormat.dateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'SSS'Z'"
+                        
+                        let dateStampFormatted = dateFormat.dateFromString(dateStamp)
+                        
+                        let dateStampWithInterval = dateStampFormatted?.dateByAddingTimeInterval(interval)
+                        
+                        let newDateStamp = dateFormat.stringFromDate(dateStampWithInterval!)
+                        
+                        println(newDateStamp)
+                        
+                        let urlForValidateTime = "\(self.commonURL)/validatetime"
+                        let validateDict: Dictionary = ["token": "YwUoY4pc6I","datestamp": newDateStamp ] as Dictionary
+                        
+                        
+                        Alamofire.request(.POST, urlForValidateTime, parameters: validateDict,encoding:.JSON).responseJSON {
+                            (request, response, json, error) in
+                            if(error != nil) {
+                                println("Error: \(error)")
+                                println(request)
+                                println(response)
+                            }
+                            else {
+                                println("Success: \(urlForValidateTime)")
+                                println(request)
+                                var json = JSON(json!)
+                                //if let data = json["result"].stringValue as String? {
+                                    println(json)
+                                //}
+                            }
+                            
+                        }
+                        
+                    }
+                }
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +86,5 @@ class Stage4ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
